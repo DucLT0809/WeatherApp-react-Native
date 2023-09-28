520d24781123f2ec83bdef9e4988a22102c3befc
Đức Lê Trung <work.duclt@gmail.com>
2023-09-28 18:07:29 +0700

    commit truoc khi doi ten

--- android/app/src/main/java/com/weatherappv2/MainActivity.java
+++ android/app/src/main/java/com/weatherappv2/MainActivity.java
@@ -13,7 +13,7 @@ public class MainActivity extends ReactActivity {
    */
   @Override
   protected String getMainComponentName() {
-    return "WeatherAppv2";
+    return "Nuôi em";
   }
 
   /**
--- ios/NuôiemTests/NuôiemTests.m
+++ ios/NuôiemTests/NuôiemTests.m
@@ -7,11 +7,11 @@
 #define TIMEOUT_SECONDS 600
 #define TEXT_TO_LOOK_FOR @"Welcome to React"
 
-@interface WeatherAppv2Tests : XCTestCase
+@interface NuôiemTests : XCTestCase
 
 @end
 
-@implementation WeatherAppv2Tests
+@implementation NuôiemTests
 
 - (BOOL)findSubviewInView:(UIView *)view matching:(BOOL (^)(UIView *view))test
 {
